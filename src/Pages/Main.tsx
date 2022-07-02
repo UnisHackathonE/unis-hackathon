a3795fac6cb2904f497697edf77f683ec7b3cf61
daieeen <101250985+daieeen@users.noreply.github.com>
2022-07-02 17:29:37 +0900

    7월 2일 17:29 커밋

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
-import {Wrapper} from "../Style/Main";
+import React from "react";
+import { Wrapper } from "../Style/Main";
+import { YellowStarButton, RedStarButton,
+  CoralStarButton, BlueClamButton, PinkClamButton,
+  OctopusButton, CoralButton, ShellButton,
+  PinkStarButton, FishButton, Title
+} from "../Style/style";
+
+import YStar from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 004.png";
+import RStar from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 005.png";
+import CStar from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 006.png";
+import BClam from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-59 011.png";
+import PClam from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-59 013.png";
+import Octopus from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 005.png";
+import Coral from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 002.png";
+import Shell from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-59 010.png";
+import PStar from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-58 003.png";
+import Fish from "../Assets/KakaoTalk_Photo_2022-07-02-15-00-59 009.png";
+import { First, Second, Third, Fourth  } from "../Style/style";
 
 const Main = () => {
   return (
-      <Wrapper>
-        메인
-      </Wrapper>
-  )
-}
+    <>
+    <Title>
+      Recommen For ME
+    </Title>
+      <First>
+        <YellowStarButton src={YStar} alt="" />
+      </First>
+
+      <Second>
+        <RedStarButton src={RStar} alt="" />
+        <CoralStarButton src={CStar} alt="" />
+      </Second>
+
+      <Third>
+        <BlueClamButton src={BClam} alt="" />
+        <PinkClamButton src={PClam} alt="" />
+        <CoralButton src={Coral} alt="" />
+      </Third>
+
+      <Fourth>
+        <OctopusButton src={Octopus} alt="" />
+        <ShellButton src={Shell} alt="" />
+        <PinkStarButton src={PStar} alt="" />
+        <FishButton src={Fish} alt="" />
+      </Fourth>
+    </>
+  );
+};
 
 export default Main;
-
---
+++ src/Style/style.tsx
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+
+
+export const Title = styled.div`
+margin
+`
+
+export const First = styled.div`
+margin: 150px 0px 40px 160px
+
+`
+export const Second = styled.div`
+display: flex;
+margin: 40px 0px 40px 120px
+`
+export const Third = styled.div`
+margin: 40px 0px 40px 80px
+
+`
+export const Fourth = styled.div`
+margin: 40px 0px 40px 30px
+`
+
+
+
+
+
+export const YellowStarButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+`
+
+export const RedStarButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px; 
+`
+export const CoralStarButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+`
+export const BlueClamButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+export const PinkClamButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+export const OctopusButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+export const CoralButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+
+export const ShellButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+export const PinkStarButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
+export const FishButton = styled.img`
+    border-radius: 70%;
+    background-color: white;
+    height: 80px;
+    width: 80px;
+    margin: 
+`
